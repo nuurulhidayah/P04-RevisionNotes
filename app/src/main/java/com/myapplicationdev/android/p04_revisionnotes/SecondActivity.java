be0c05f1aa0ea3e77836ac5a0db1b118474f5b34
Nuurulhidayah <19040408@myrp.edu.sg>
2021-05-06 16:10:59 +0800

    Commit #2

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -1,16 +1,18 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
-
+	ListView lv;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//TODO implement the Custom ListView
+		lv = findViewById(R.id.lv);
 
+		
 	}
-
-
 }
