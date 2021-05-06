897b743a4f40fbbfe6c43a50166e8a0779a23f54
li tong <lililililitongg@gmail.com>
2021-05-06 15:55:19 +0800

    commit

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -60,6 +60,27 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	public ArrayList<Note> getAllNotes() {
 		//TODO return records in Java objects
+		ArrayList<Note> notes = new ArrayList<Note>();
+		String selectQuery = "SELECT " + COLUMN_ID + ", "
+				+ COLUMN_NOTECONTENT + ", "
+				+ COLUMN_STARS
+				+ " FROM " + TABLE_NOTE;
+
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.rawQuery(selectQuery, null);
+
+		if (cursor.moveToFirst()) {
+			do {
+				int id = cursor.getInt(0);
+				String noteContent = cursor.getString(1);
+				int stars = cursor.getInt(2);
+				Note obj = new Note(id, noteContent, stars);
+				notes.add(obj);
+			} while (cursor.moveToNext());
+		}
+		cursor.close();
+		db.close();
+		return notes;
 	}
 
     public ArrayList<String> getNoteContent() {
@@ -68,7 +89,8 @@ public class DBHelper extends SQLiteOpenHelper {
 		// Create an ArrayList that holds String objects
         ArrayList<String> notes = new ArrayList<String>();
         // Select all the notes' content
-        String selectQuery = "";
+		String selectQuery = "SELECT " + COLUMN_NOTECONTENT
+				+ " FROM " + TABLE_NOTE;
 
         // Get the instance of database to read
         SQLiteDatabase db = this.getReadableDatabase();
@@ -79,8 +101,7 @@ public class DBHelper extends SQLiteOpenHelper {
             // Loop while moveToNext() points to next row and returns true;
             // moveToNext() returns false when no more next row to move to
             do {
-
-
+				notes.add(cursor.getString(0));
             } while (cursor.moveToNext());
         }
         // Close connection
