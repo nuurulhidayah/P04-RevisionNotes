5826dd29822b921752cd7765086a5e7a3322d4ca
li tong <lililililitongg@gmail.com>
2021-05-06 16:09:29 +0800

    commit

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/MainActivity.java
@@ -1,14 +1,25 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    EditText revisionNote;
+    RadioGroup rg;
+    Button btnInsert, btnShowList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        revisionNote = (EditText) findViewById(R.id.editTextNote);
+        rg = findViewById(R.id.radioGroupStars);
+        btnInsert = (Button) findViewById(R.id.buttonInsertNote);
+        btnShowList = (Button) findViewById(R.id.buttonShowList);
     }
 }
