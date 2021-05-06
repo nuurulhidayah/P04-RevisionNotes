3552dec7e97b15aa9e9c26b50373843d70c54927
Nuurulhidayah <19040408@myrp.edu.sg>
2021-05-06 15:48:11 +0800

    Commit and push #2

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -12,9 +12,8 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
 	//TODO Define the Database properties
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = ;
-
+	private static final String DATABASE_NAME = "note.db";
+	private static final int DATABASE_VERSION = 1;
 
 	public DBHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,7 +22,7 @@ public class DBHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		//TODO CREATE TABLE Note
-
+		String createTableSql = "CREATE TABLE "  + TABLE_NOTE;
 	}
 
 	@Override
